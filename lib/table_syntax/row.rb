0d091e578ee5f703446ce4deaada5b03b9061a14
Jan Vlnas <git@jan.vlnas.cz>
2015-07-04 14:36:09 +0200

    Extract classes from rspec-parameterized

---
+++ lib/table_syntax/row.rb
@@ -0,0 +1,23 @@
+#
+# Based on RSpec::Parameterized::Table::Row from https://github.com/tomykaira/rspec-parameterized
+# Copyright 2012-2015 tomykaira
+#
+module TableSyntax
+  class Row
+    def initialize(param)
+      @params = [param]
+    end
+
+    def add_param(param)
+      @params << param
+    end
+
+    def to_a
+      @params
+    end
+
+    def to_params
+      [@params]
+    end
+  end
+end
