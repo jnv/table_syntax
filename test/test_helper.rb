cb12ebee3c379fedb3ba6243140712beb4b8619e
Jan Vlnas <git@jan.vlnas.cz>
2015-07-04 16:03:13 +0200

    Tests: Use spec reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'table_syntax'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
