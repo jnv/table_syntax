17119be1627c10f279cdfdcbf8984d067402fd1a
Jan Vlnas <git@jan.vlnas.cz>
2015-07-05 17:29:31 +0200

    Implement TableSyntax.build

--- lib/table_syntax.rb
+++ lib/table_syntax.rb
@@ -1,5 +1,12 @@
 require "table_syntax/version"
+require "table_syntax/table_parser"
+require "table_syntax/header_table"
 
 module TableSyntax
-  # Your code goes here...
+
+  def self.build(*headers, &blk)
+    table = TableParser.parse(blk)
+    HeaderTable.new(headers, table)
+  end
+
 end
--- test/table_syntax_test.rb
+++ test/table_syntax_test.rb
@@ -4,4 +4,18 @@ class TableSyntaxTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::TableSyntax::VERSION
   end
+
+  def test_build
+
+    t = TableSyntax.build(:col1, :col2) do
+      :a | :b
+    end
+
+    assert_kind_of Enumerable, t
+    t.each do |row|
+      assert_equal :a, row.col1
+      assert_equal :b, row.col2
+    end
+
+  end
 end
