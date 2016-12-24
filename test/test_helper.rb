d38b09300ef1c4d1551ff3d187ec133f1d70e5ab
Jan Vlnas <git@jan.vlnas.cz>
2016-12-25 00:25:59 +0100

    Update CodeClimate test reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
+  require 'simplecov'
+  SimpleCov.start
 end
 
 require 'minitest/autorun'
